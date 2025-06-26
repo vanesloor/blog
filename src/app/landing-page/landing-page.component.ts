2dc8f27e1c50c8f4e13a7e4be3c961e3a0423e73
Vanessa Lopez <vanesloor@gmail.com>
2025-06-26 10:07:45 -0400

    updates

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
@@ -8,4 +9,11 @@ import { Component } from '@angular/core';
 })
 export class LandingPageComponent {
 
+  constructor(
+    private _router: Router
+  ){}
+
+  public routeToContact(): void{
+    this._router.navigate(['/contact']);
+  }
 }
